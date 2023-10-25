5543e75c8b071b09e63f247ec432d9d31f164177
Egorsvetlichny <fkhor@inbox.ru>
2023-10-25 16:55:04 +0300

    Solve leetcode task 13: roman_to_integer from top_interview_150 block

---
+++ leetcode/top_interview_150/roman_to_integer.py
@@ -0,0 +1,68 @@
+# task 13. Roman to Integer
+
+# Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+# Symbol       Value
+# I             1
+# V             5
+# X             10
+# L             50
+# C             100
+# D             500
+# M             1000
+
+# For example, 2 is written as II in Roman numeral, just two ones added together.
+# 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+# Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII.
+# Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
+# The same principle applies to the number nine, which is written as IX.
+
+# There are six instances where subtraction is used:
+# I can be placed before V (5) and X (10) to make 4 and 9.
+# X can be placed before L (50) and C (100) to make 40 and 90.
+# C can be placed before D (500) and M (1000) to make 400 and 900.
+
+# Given a roman numeral, convert it to an integer.
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        dict_roman_int = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        res = 0
+
+        for i in range(len(s)):
+            if dict_roman_int[s[i]] > dict_roman_int[s[i - 1]] and i > 0:
+                res += dict_roman_int[s[i]] - dict_roman_int[s[i - 1]] * 2
+            else:
+                res += dict_roman_int[s[i]]
+
+        return res
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    # test case 1
+    s = "III"
+    assert solution.romanToInt(s) == 3
+
+    # test case 2
+    s = "LVIII"
+    assert solution.romanToInt(s) == 58
+
+    # test case 3
+    s = "MCMXCIV"
+    assert solution.romanToInt(s) == 1994
